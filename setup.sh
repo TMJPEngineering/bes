030eed3dd403d1f62bc2730d03d8ca128e1da1bc
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-10-23 16:54:06 +0800

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,23 +1,11 @@
 envFile=".env"
 nodeDir="node_modules"
-bowerDir="bower_components"
 
 # If the value of nodeDir is not exist then run the command
 if [[ ! -d "$nodeDir" ]]; then
     echo "Installing npm libraries..."
     npm install
-fi
-
-# If bower command is not exist, then install using npm
-if ! bowerCommand="$(type -p "bower")" || [ -z "$bowerCommand" ]; then
-    echo "Install bower command..."
-    npm install -g bower
-fi
-
-# If the value of bowerDir is not exist then run the command
-if [[ ! -d "$bowerDir" ]]; then
-    echo "Installing bower libraries..."
-    bower install
+    npm install -D
 fi
 
 # If the value of envFile is not exist then run the command
@@ -27,7 +15,6 @@ if [[ ! -f "$envFile" ]]; then
 fi
 
 printf "\e[32m ✔ npm libraries \n" # Print in green color
-printf "\e[32m ✔ bower components \n" # Print in green color
 printf "\e[32m ✔ env variables \n" # Print in green color
 printf "\033[0m" # Reset colors back to normal
 
