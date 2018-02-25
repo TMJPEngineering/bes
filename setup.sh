54f9eff18b204401d04bb890fee4eac69b65fcec
John Conrad Lumacad <jcmlumacad@tmj.ph>
2018-02-25 23:34:34 +0800

    Update setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@ envFile=".env"
 nodeDir="node_modules"
 
 # If the value of nodeDir is not exist then run the command
-if [[ ! -d "$nodeDir" ]]; then
-    echo "Installing npm libraries..."
-    npm install
-    npm install -D
-fi
+# if [[ ! -d "$nodeDir" ]]; then
+#    echo "Installing npm libraries..."
+#    npm install
+#    npm install -D
+# fi
 
 # If the value of envFile is not exist then run the command
 if [[ ! -f "$envFile" ]]; then
@@ -19,4 +19,4 @@ printf "\e[32m ✔ env variables \n" # Print in green color
 printf "\033[0m" # Reset colors back to normal
 
 npm run dev # Run production mode for webpack
-npm start # Starting server
+babel-node server # Starting server
