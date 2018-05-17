e414e817181ba87e9de7b75d180d59622dd86d95
John Conrad Lumacad <johnconradl@outlook.com>
2018-05-17 16:21:33 +0800

    Enhanced database config

--- bootstrap/database-connection/mongoose.js
+++ bootstrap/database-connection/mongoose.js
@@ -5,16 +5,22 @@ import sharedSchema from '~/modules/Shared/Models/SharedSchema';
 
 export const mongoose = (connection, seeders = false) => {
     const Mongoose = connection.driver;
-    const database = Mongoose.connection;
-    const credentials = connection.password
-        ? connection.username + ':' + connection.password
-        : connection.username;
-    const uri = connection.uri
-        ? connection.uri
-        : connection.host + '://' + credentials + '/' + connection.database;
+    const databaseConnection = Mongoose.connection;
 
-    database.on('error', error => logger.error(error));
-    database.once('open', () => {
+    let credentials = '';
+
+    if (connection.username || connection.password) {
+        credentials = connection.password ?
+            `${connection.username}:${connection.password}@` :
+            `${connection.username}@`;
+    }
+
+    const single_host = connection.host + ':' + connection.port;
+    const database = `/${connection.database}`;
+    const uri_format = connection.connection_format + credentials + single_host + database + connection.options;
+
+    databaseConnection.on('error', error => logger.error(error));
+    databaseConnection.once('open', () => {
         console.log('Connected to database');
         if (seeders && config.app.env !== 'production') {
             // Drop database
@@ -26,7 +32,7 @@ export const mongoose = (connection, seeders = false) => {
     });
 
     Mongoose.Promise = bluebird;
-    Mongoose.connect(uri, {
+    Mongoose.connect(uri_format, {
         useMongoClient: true
     });
     // Added updated_at and created_at in every collection
